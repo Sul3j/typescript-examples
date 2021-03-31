8599a8d36ac0ad53a38f5d6aa7482f2af6e50054
Szymon Sulejczak <55898132+Sul3j@users.noreply.github.com>
2021-03-31 14:56:21 +0200

    ADD: Examples description

--- basic-types/index.ts
+++ basic-types/index.ts
@@ -1,29 +1,29 @@
-// basic types
+// basic types (boolean, number, string, null, undefined)
 const isBoolean: boolean = true;
 const decimalNumber: number = 10;
 const exampleString: string = "example string";
 const nullVar: null = null;
 const undefinedVar: undefined = undefined;
 
-// union types
+// union types (allows the use of several types)
 const age: number | string = 23;
 const isOld: string | boolean = true;
 
-// array types
+// array types (specifies the type of data in the array)
 const namesArray: string[] = ["Asia", "Basia", "Adam"];
 const isGoodArray: boolean[] = [true, false, true];
 const agesArray: number[] = [24, 45, 18];
 
-//or
+//or (another way of saving the data type in an array)
 const stringArray: Array<string> = ["okno", "patyk", "jabłko"];
 const booleanArray: Array<boolean> = [false, true, false];
 const numberArray: Array<number> = [1, 2, 3];
 
-//union type array
+//union type array (allows you to enter several types of data into an array)
 const peopleArray: Array<string | number> = ["Adam", 24, "Kasia", 45];
 const allArray: Array<boolean | number> = [false, 34, false, 45];
 
-// tuple 
+// tuple (an array with a specified number of elements and types)
 const tuple: [number, string] = [24, "Adam"];
 
 // enum (number and string)
@@ -40,7 +40,7 @@ enum stringExample {
     EVENT_CHANGE = 'change',
 }
 
-// unknown type
+// unknown type 
 let uknownExample: unknown = true;
 
 // any type 
