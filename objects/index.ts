80a9ebf8869e21592a3c214a45ace8a7bb3a177a
Szymon Sulejczak <55898132+Sul3j@users.noreply.github.com>
2021-04-08 09:27:29 +0200

    ADD: User object declaration

--- objects/index.ts
+++ objects/index.ts
@@ -1,9 +1,17 @@
-const user = {} as { age: number | string, names?: { firstName: string, lastName: string } };
+// const user = {} as { age: number | string, names?: { firstName: string, lastName: string } };
 
-user.age = 20;
-user.names = {} as { firstName: string, lastName: string };
-user.names.firstName = 'Szymon';
-user.names.lastName = 'Sulejczak';
+// user.age = 20;
+// user.names = {} as { firstName: string, lastName: string };
+// user.names.firstName = 'Szymon';
+// user.names.lastName = 'Sulejczak';
+
+const user = {
+    age: 20, 
+    names: {
+        firstName: 'Szymon',
+        lastName: 'Sulejczak',
+    }
+}
 
 function showUserInfo(user: { age: number | string, names?: { firstName: string, lastName: string}}) {
     const { age, names: { firstName, lastName } = {firstName: 'Unknown', lastName: 'user'}} = user;
