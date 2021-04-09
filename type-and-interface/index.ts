3d441f8066a0ff2f0f25d0aa5743bc955a0fc8c1
Szymon Sulejczak <55898132+Sul3j@users.noreply.github.com>
2021-04-09 05:55:54 +0200

    ADD: UserTypes and CommentsTypes type

---
+++ type-and-interface/index.ts
@@ -0,0 +1,8 @@
+type UserTypes = 'Admin' | 'User';
+type CommentsTypes = 'Public' | 'Private';
+
+let myUserType: UserTypes = 'Admin';
+let myCommentType: CommentsTypes = 'Public';
+
+myUserType = 'User';
+
