9e39e6e1658a1a5f8efed5dc513726e4fbd22c55
Szymon Sulejczak <55898132+Sul3j@users.noreply.github.com>
2021-04-09 08:12:23 +0200

    ADD: Required example

--- utility-types/index.ts
+++ utility-types/index.ts
@@ -32,3 +32,23 @@ const newNameOfPerson = {
 
 updatePerson(0, newNameOfPerson);
 anotherUpdate(szymon, szymonUpdated);
+
+type Emplyee = {
+    id: number;
+    firstName: string;
+    lastName: string;
+    jobPosition?: string;
+}
+
+function newWorker(newWorker: Required<Emplyee>) {}
+
+// In Required type all items are required
+
+const newFrontend: Required<Emplyee> = {
+    id: 89,
+    firstName: 'Aleksander',
+    lastName: 'Sulejczak',
+    jobPosition: 'frontend',
+}
+
+newWorker(newFrontend);
