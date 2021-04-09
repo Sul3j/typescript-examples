d09f2be0d53a966222aea142614e4f6c5c00fe50
Szymon Sulejczak <55898132+Sul3j@users.noreply.github.com>
2021-04-09 06:01:33 +0200

    ADD: BiggerType and SmallerType type objects and showProperty function

--- type-and-interface/index.ts
+++ type-and-interface/index.ts
@@ -4,5 +4,20 @@ type CommentsTypes = 'Public' | 'Private';
 let myUserType: UserTypes = 'Admin';
 let myCommentType: CommentsTypes = 'Public';
 
-myUserType = 'User';
+// myUserType = 'User';
 
+type AccessMode = 1 | 2 | 3 | 4 | 5;
+
+type BiggerType = {
+    commonProperty: string;
+    additionalProperty: string;
+}
+
+type SmallerType = {
+    commonProperty: string;
+    anotherOneProperty: number;
+}
+
+function showProperty(someObject: SmallerType) {
+    return someObject.commonProperty;
+}
